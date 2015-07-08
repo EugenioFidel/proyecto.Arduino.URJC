50ff6324e6fe6f6b6d70dfac7518f81b2f86cbfa
EugenioFidel <eugeniofidel@gmail.com>
2015-07-08 11:00:50 +0200

    primer commit

---
+++ Codigo/servidor/servidor.ino
@@ -0,0 +1,143 @@
+#include <Twitter.h>
+#include <SPI.h>              
+#include <Ethernet.h>
+#include <DHT.h>
+
+#define MAXTEMP  28
+#define MAXHUME  20
+#define HUMVAL    2
+#define HUMAL     3
+#define TEMPVAL   5
+#define TEMPAL    4
+#define PINDHT    7
+
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192,168,1,144); // Ip del servidor 192.168.1.144 
+IPAddress myDns(8,8,8,8); 
+EthernetServer server(80);  // Arrancamos el servidor en el puerto estandard 80
+DHT dht( PINDHT, DHT11);
+char buffer[140];
+Twitter twitter("3095700296-qHmjpweAY2Q2p3pxIUEU49QmJTy6ExXuFXetc27");  //Token del que nos provee Twitter para identificarnos como usuarios
+
+void setup()
+{
+   pinMode(HUMVAL,OUTPUT);
+   pinMode(HUMAL,OUTPUT);
+   pinMode(TEMPVAL,OUTPUT);
+   pinMode(TEMPAL,OUTPUT);
+   dht.begin();
+   Serial.begin(9600);
+   while (!Serial) ;        
+
+   Ethernet.begin(mac, ip,myDns);
+   delay(3000);
+   server.begin();          // Inicia el servidor web
+   Serial.print("Servidor Web en la direccion: ");
+   Serial.println(Ethernet.localIP());
+}
+
+void loop(){
+  EthernetClient client = server.available();  // Cuando se produce una petición al puerto 80 de la ip local 192.168.1.144   
+   if (client) 
+    { Serial.println("CONECTADO");
+      boolean currentLineIsBlank = true;  // Las peticiones HTTP finalizan con linea en blanco
+      while (client.connected())
+        { if (client.available())
+             {  char c = client.read();
+                Serial.write(c);   // mostramos gestión http por la consola
+                // A partir de aquí mandamos nuestra respuesta
+               if (c == '\n' && currentLineIsBlank) 
+                  {   // Enviar una respuesta tipica
+                      client.println("HTTP/1.1 200 OK");             
+                      client.println("Content-Type: text/html");
+                      client.println("Connection: close");
+                      client.println("Refresh: 20");            // Actualizar cada minuto
+                      client.println();
+                      client.println("<!DOCTYPE HTML>");
+                      client.println("<html>");
+
+                      float h = dht.readHumidity();           // Leer el sensor
+                      float t = dht.readTemperature();
+                      Serial.println(t);
+                      Serial.println(h);
+                      // Creamos la página web
+                      client.print("<head><title>Observacion ambiental</title></head>");
+                      client.print("<body><IMG SRC=\"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTUN_2J_jYXp4s96GZEAd16L0Wk-wfY7bWBiYuzOtH29Eqv2YaU\">");
+                      client.print("<h1> Humedad y temperatura en el local</h1><p>Temperatura: ");
+                      client.print(t);     // Aqui va la temperatura
+                      client.print(" grados Celsius</p>");
+                      client.print("<p>Humedad:  ");
+                      client.print(h);    // Aqui va la humedad
+                      client.print(" por ciento</p>");
+                      client.print("<p><em> La pagina se actualiza cada 20 segundos.</em></p></body></html>");
+                      GestionarLeds(t,h);
+                        
+                      break;
+                }
+            if (c == '\n')
+                currentLineIsBlank = true;          // nueva linea
+            else if (c != '\r')
+                currentLineIsBlank = false;
+          }
+        }
+        }else{
+          
+             float h = dht.readHumidity();           // Leer el sensor
+             float t = dht.readTemperature();
+             GestionarLeds(t,h);
+         }
+     delay(10);         // Para asegurarnos de que los datos se envia
+     client.stop();     // Cerramos la conexion
+     delay(20000);
+ }  
+
+
+//Procedimiento que recibe un Array de crctes y lo envía a Twitter usando la libreria Twitter.h 
+void tweet(char msg[]){
+  char textoTweet[144];
+  sprintf(textoTweet,"%s",msg);
+  if (twitter.post(textoTweet)){    
+    int status = twitter.wait(&Serial);
+    if (status == 200){
+      Serial.println(F("Tweet enviado"));
+    }else{
+      Serial.print(F("Fallo subida tweet:cdg:"));
+      Serial.println(status);
+    }
+  }else{
+    Serial.println(F("Fallo conexión."));
+  }
+}
+
+void GestionarLeds(float t,float h){
+  if(t>MAXTEMP){
+                         digitalWrite(TEMPVAL,LOW);
+                         digitalWrite(TEMPAL,HIGH);
+                         char temperatura[6];
+                         dtostrf(t,5, 2, temperatura);
+                         temperatura[5]='\0';                                                 
+                         sprintf(buffer,"Alarma ambiental. Temperatura %s grados.- cod - %d",temperatura,millis());
+                         Serial.println(buffer);
+                         delay (1000);
+                         tweet(buffer);
+  }else{
+     digitalWrite(TEMPVAL,HIGH); //PONEMOS HUMEDAD EN VALIDO
+     digitalWrite(TEMPAL,LOW);  
+   }
+  
+   if(h>MAXHUME){
+                         digitalWrite(HUMVAL,LOW);
+                         digitalWrite(HUMAL,HIGH);
+                         char humedad[6];
+                         dtostrf(h,5, 2, humedad);
+                         humedad[5]='\0';                                                 
+                         sprintf(buffer,"Alarma ambiental. Humedad %s por ciento.- cod - %d",humedad,millis());
+                         Serial.println(buffer);
+                         delay (1000);
+                         tweet(buffer);
+   }else{
+      digitalWrite(HUMVAL,HIGH); //PONEMOS HUMEDAD EN VALIDO
+      digitalWrite(HUMAL,LOW);  
+   }
+}
---
+++ Codigo/unTweetN/unTweetN.ino
@@ -0,0 +1,162 @@
+
+
+/* Control de Ip's en red local:
+ *Programa que controla si las ip's presentes en un LAN se encuentran registradas en una tabla de control.
+ *De no estarlo se genera un mensaje de alerta que se envía a la red social Twitter, con expresión de la IP
+ *no registrada
+ */
+    #include <SD.h>
+    #include <SPI.h>
+    #include <Ethernet.h>    
+    #include <Twitter.h>
+    #include <ICMPPing.h>
+                                                       
+   #define FBYTE 192
+   #define SBYTE 168
+   #define TBYTE 1
+   #define FRBYTE 144
+   
+   #define NUMPAQUETES 4
+   
+   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };                    //mac para la placa Arduino
+   byte ipConection[]  = {FBYTE,SBYTE,TBYTE,FRBYTE};                       //IP para la placa Arduino
+   IPAddress myDns(8,8,8,8);                                               //Servidor DNS, establecemos la dirección de DNS's de google.com     
+   Twitter twitter("3095700296-qHmjpweAY2Q2p3pxIUEU49QmJTy6ExXuFXetc27");  //Token del que nos provee Twitter para identificarnos como usuarios
+   SOCKET pingSocket = 0;                                                  //socken para establecer comunicación en la LAN
+   char buffer [64];                                                       //un array de caracteres para envio de mensajes a la consola Serie
+   ICMPPing ping(pingSocket, (uint16_t)random(0, 255));                    //objeto de la clase ICMPPing para hacer pings en la red local
+   EthernetClient client;
+   int chipSelect=4;                                                       //este puerto será utilizado para la conexión con la tarjeta SD
+   int ipsLeidas[5][4];                                                    //un array de enteros de 5x4 que almacenará un máximo de cinco direcciones ip autorizadas en la lan
+   int numIps=0;                                                           //número de ips leidas del fichero de ips autorizadas
+
+
+   void setup(){
+      Ethernet.begin(mac,ipConection,myDns); 
+      delay(100);
+      Serial.begin(9600);
+      delay(1000);
+      Serial.print(F("Conectado con IP: "));
+      for (byte i = 0; i < 4; i++){
+        Serial.print(ipConection[i], DEC);
+        //Serial.print(ipConection[i]);
+        Serial.print(F(".")); 
+      }
+    Serial.println();
+     // Si ha habido error al leer la tarjeta informamos por el puerto serie.
+    if (!SD.begin(chipSelect)){
+      Serial.println(F("Error al leer la tarjeta."));
+    }
+  // Abrimos el archivo.
+	File dataFile = SD.open("ipsAuth3.txt"); 
+	if (dataFile) {                 //abro if
+		//Mientras el fichero tenga contenido
+		while (dataFile.available()) {      //abro while
+			for(byte i=0;i<4;i++){                //abro for
+				char crct;           
+				byte index=0; 
+				char valor[4];        
+				while((crct=dataFile.read())!='.'){   //abro while
+					valor[index]=crct;
+					valor[index+1]='\0';
+					index++; 
+				}                                    //cierro while
+				ipsLeidas[numIps][i]=atoi(valor);            
+			}                                    //cierro for
+			Serial.print(F("ip leida del fichero: "));
+			Serial.print(ipsLeidas[numIps][0]);
+			Serial.print(F("."));
+			Serial.print(ipsLeidas[numIps][1]);
+			Serial.print(F("."));
+			Serial.print(ipsLeidas[numIps][2]);
+			Serial.print(F("."));
+			Serial.println(ipsLeidas[numIps][3]);   
+			numIps++;
+		}	//cierro while 
+		dataFile.close();	//cierro el fichero
+        }else {
+		Serial.println(F("Error al abrir .txt"));    
+	} //cierro if
+
+        
+}
+ 
+void loop()
+{  
+  for(int i=1;i<256;i++){ //for
+      IPAddress pingAddr(FBYTE,SBYTE,TBYTE,i);
+      ICMPEchoReply echoReply = ping(pingAddr, NUMPAQUETES);
+      if (echoReply.status == SUCCESS){   //if ping resulto  
+            sprintf(buffer,"Reply[%d] from:%d.%d.%d.%d:bytes=%d time=%ldms TTL=%d",
+            echoReply.data.seq,
+            echoReply.addr[0],
+            echoReply.addr[1],
+            echoReply.addr[2],
+            echoReply.addr[3],
+            REQ_DATASIZE,
+            millis() - echoReply.data.time,
+            echoReply.ttl);
+           
+            if(!(comprobarIP(echoReply.addr))){    // si la ipE está en el fichero de ips           
+               Serial.println(F("Ip NO autorizada"));   
+               sprintf(buffer,"Ip no autorizada en:%d.%d.%d.%d, cd.-%d", FBYTE,SBYTE,TBYTE,i,millis()); 
+               tweet(buffer);
+            }else{
+                    Serial.println(F("IP AUTORIZADA"));
+            } //cierre if  si la ipE está en el fichero de ips
+        
+      }else{
+        sprintf(buffer, "Echo request failed;%d.%d.%d.%d",FBYTE,SBYTE,TBYTE,i);
+      }// cierre if ping resuelto
+      //imprimo el buffer
+      Serial.println(buffer);      
+  } //cierre for
+ 
+  
+}
+
+  boolean comprobarIP(IPAddress a){
+       
+        Serial.print(F("\n\nPing atendido en ip: "));
+        //Serial.print(echoReply[0]);
+        Serial.print(a[0]);
+        Serial.print(F("."));
+        //Serial.print(echoReply[1]);
+        Serial.print(a[1]);
+        Serial.print(F("."));
+        //Serial.print(echoReply[2]);
+        Serial.print(a[2]);
+        Serial.print(F("."));
+        //Serial.print(echoReply[3]);
+        Serial.print(a[3]);
+        Serial.println();
+	for(byte i=0;i<numIps;i++){
+		boolean chivato=true;
+		for(byte j=0;j<4;j++){
+			if(!((a[j])==ipsLeidas[i][j])){
+				chivato=false;
+			}
+		}
+		if(chivato){
+			return chivato;
+		}
+	}
+	return false;
+ }
+
+//Función que recibe un Array de crctes y lo envía a Twitter usando la libreria Twitter.h 
+void tweet(char msg[]){
+  char textoTweet[144];
+  sprintf(textoTweet,"%s",msg);
+  if (twitter.post(textoTweet)){    
+    int status = twitter.wait(&Serial);
+    if (status == 200){
+      Serial.println(F("Tweet enviado"));
+    }else{
+      Serial.print(F("Fallo subida tweet:cdg:"));
+      Serial.println(status);
+    }
+  }else{
+    Serial.println(F("Fallo conexión."));
+  }
+}
